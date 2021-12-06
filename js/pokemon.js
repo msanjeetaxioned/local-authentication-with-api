8c90b083509e60b395e0f95804373408073326c3
Sanjeet Mistry <sanjeetm@axioned.com>
2021-12-06 18:30:52 +0530

    Add 'Header' with signout in 'Pokemon' page

--- js/pokemon.js
+++ js/pokemon.js
@@ -20,6 +20,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function getPokemonData() {
+        const headerContent = body.querySelector(".header-content");
+        const usernameInHeader = headerContent.querySelector("p");
+        const signOut = headerContent.querySelector("a");
+
+        usernameInHeader.innerText = "Hello! " + localStorage.getItem("user-name");
+
+        signOut.addEventListener("click", function() {
+            localStorage.removeItem("user-name");
+            window.location.replace("http://127.0.0.1:5500/login.html");
+        });
+
         const xhttp = new XMLHttpRequest();
         const url = `https://pokeapi.co/api/v2/pokemon/${sessionStorage.getItem("pokemon-id")}/`;
         xhttp.open("GET", url);
